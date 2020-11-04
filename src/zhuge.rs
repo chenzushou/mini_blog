c5180ace4f9c1b86b58a39d0d3b37e7b5c30bdcc
chenzushou <chenzushou18@163.com>
2020-11-04 15:13:46 +0800

    完善一些数据库更新语句

--- src/zhuge.rs
+++ src/zhuge.rs
@@ -3,9 +3,11 @@ use actix_web::{web,Error as AWError};
 use failure::Error;
 use std::{thread::sleep, time::Duration};
 use futures::{Future,TryFutureExt};
+use rusqlite::{Statement,NO_PARAMS};
+use std::iter::Iterator;
 pub type Pool = r2d2::Pool<r2d2_sqlite::SqliteConnectionManager>;
 pub type Connection = r2d2::PooledConnection<r2d2_sqlite::SqliteConnectionManager>;
-type ZugeResult = Result<Zhuge,rusqlite::Error>;
+type ZugeResult = Result<Vec<Zhuge>,rusqlite::Error>;
 //猪哥类
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Zhuge{
@@ -19,7 +21,7 @@ pub fn execute(
     pool:&Pool,
     action:action,
 )->
-impl Future<Output=Result<Zhuge,AWError>>{
+impl Future<Output=Result<usize,AWError>>{
     let pool=pool.clone();
     web::block(move||{
         sleep(Duration::from_secs(2));
@@ -31,13 +33,40 @@ impl Future<Output=Result<Zhuge,AWError>>{
     })
     .map_err(AWError::from)
 }
-fn add_score(conn:Connection) ->ZugeResult {
-    let stmt=conn.prepare(
+fn get_score(conn:Connection) ->ZugeResult {
+    let mut  stmt=conn.prepare(
         "
         SELECT * FROM zuge_score
         ",
     )?;
-    
+    stmt.query_map(NO_PARAMS,|row| {
+        Ok(
+            Zhuge{
+                point:row.get(0)?,
+            }
+        )
+    }).and_then(Iterator::collect)
+}
+fn get_zuge_score(mut statement:Statement)->ZugeResult{
+    statement.query_map(NO_PARAMS,|row| {
+        Ok(
+            Zhuge{
+                point:row.get(0)?,
+            }
+        )
+    }).and_then(Iterator::collect)
+}
+fn add_score(conn:Connection)->Result<usize,rusqlite::Error>{
+    conn.execute(
+        "UPDATE zuge_score score=score+10",
+        NO_PARAMS,
+    )
+}
+fn sub_score(conn:Connection)->Result<usize,rusqlite::Error> {
+    conn.execute(
+        "UPDATE zuge_score score=score-10",
+        NO_PARAMS,
+    )
 }
 
 
