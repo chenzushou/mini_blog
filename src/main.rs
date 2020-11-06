4c27303b18f6ac5b97128d6ea318a065b86968f4
chenzushou <chenzushou18@163.com>
2020-11-06 21:03:38 +0800

    解决了json的返回问题

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use actix_web::{get,web,App,HttpServer,Responder,HttpResponse,Result,HttpRequest,middleware,Error as AWError};
+use actix_web::{App, HttpResponse, HttpServer, get, http::header::ContentType, http::{self, header::ACCESS_CONTROL_ALLOW_ORIGIN}, middleware, web::{self, route}};
 mod zhuge;
 use std::io::Write;
 use env_logger::fmt::Color;
 use chrono::Local;
-use zhuge::{Zhuge};
+use log::*;
+use zhuge::{Zhuge}; 
 
-async fn get_score(data: web::Data<Zhuge>)->impl Responder{
+fn get_score()->HttpResponse{
     let result = zhuge::get_score().unwrap();
-    HttpResponse::Ok().json(result)
+    HttpResponse::Ok().header(ACCESS_CONTROL_ALLOW_ORIGIN, "*").json(result)
 }
 
 #[actix_web::main]
@@ -27,13 +28,47 @@ async fn main()->std::io::Result<()>{
 
 
 
-    HttpServer::new(move||
+    HttpServer::new(||{
         App::new()
         .wrap(middleware::Logger::default())
+        .service(  
+        web::scope("/zugescore")
+                .route("", web::get().to(get_score))
+        )
         .service(actix_files::Files::new("/","./static/").index_file("index.html"))
-        .service(web::resource("/zugescore").route(web::post().to(get_score)))
+    }     
     )
     .bind("127.0.0.1:8080")?
     .run()
     .await
-}   
+} 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::dev::Service;
+    use actix_web::{http, test, web, App};
+
+    #[actix_rt::test]
+    async fn test_index() {
+        let mut app = test::init_service(
+            App::new().service(web::resource("/zugescore").route(web::get().to(get_score))),
+        )
+        .await;
+
+        let req = test::TestRequest::get()
+            .uri("/zugescore")
+            .to_request();
+        let resp = app.call(req).await.unwrap();
+
+        assert_eq!(resp.status(), http::StatusCode::OK);
+
+        let response_body = match resp.response().body().as_ref() {
+            Some(actix_web::body::Body::Bytes(bytes)) => bytes,
+            _ => panic!("Response error"),
+        };
+
+        assert_eq!(response_body, r##"{"point":"100"}"##);
+
+    }
+}
--- src/zhuge.rs
+++ src/zhuge.rs
@@ -3,15 +3,16 @@ use serde::{Deserialize, Serialize};
 use failure::Error;
 #[derive(Debug, Serialize, Deserialize)]
  pub struct Zhuge{
-    point:u8,
+    pub point:u8,
 }
 pub enum action{
     add,
     sub,
 }
 pub fn get_score()->Result<Zhuge,Error>{
-    let connection = sqlite::open("./blog.db").unwrap();
+    let connection = sqlite::open("blog.db").unwrap();
     let mut stmt=connection.prepare("SELECT * FROM zuge_score").unwrap();
+    stmt.next();
     Ok(    
         Zhuge{
         point:stmt.read::<i64>(0).unwrap() as u8,
